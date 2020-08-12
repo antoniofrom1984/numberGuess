a3be0acdaada34eb9aaecbb0d278eb1ebd06dc1a
antonio <antoniofrom1984@gmail.com>
2020-08-12 17:36:04 +0100

    step 3
    created and tested generateTarget and compareGuesses functions and tested them

---
+++ src/docs/script.js
@@ -0,0 +1,11 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+generateTarget = () => Math.floor(Math.random() * Math.floor(9));
+
+let myrandom = generateTarget();
+
+console.log(myrandom);
---
+++ src/script.js
@@ -0,0 +1,44 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+// make sure is a rounded number
+const generateTarget = () => Math.floor(Math.random() * Math.floor(10));
+
+const compareGuesses = (human, computer, secret) => {
+
+//calculate the least difference between guesses and computer
+
+  const humanGuess = Math.abs(human - secret);
+  const computerGuess = Math.abs(computer - secret);
+
+  if (humanGuess <= computerGuess) {
+    return true;
+  }
+  if (humanGuess > computerGuess) {
+    return false;
+  }
+
+
+}
+
+
+// // testing all cases
+// //     human wins
+// const a = compareGuesses(4,9,5);
+//     // computer wins
+// const b =compareGuesses(1,6,5);
+//     // human wins on tie
+// const c =compareGuesses(6,6,5);
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+
+const updateScore = () => {
+    
+}
